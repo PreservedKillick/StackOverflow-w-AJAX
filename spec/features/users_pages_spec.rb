9f6323260e475dad900179111d2a46515332f5c5
Drew Seminara & Angela Yen <student@epicodus.com>
2014-04-14 13:59:29 -0700

    successfully test sign in with capybara

--- spec/features/users_pages_spec.rb
+++ spec/features/users_pages_spec.rb
@@ -10,8 +10,26 @@ describe User do
     fill_in 'Password', :with => user.password
     fill_in 'Confirm password', :with => user.password
     click_button 'Create User'
-    save_and_open_page
     page.should have_content "Thanks for signing up!"
   end
 
+  it 'allows a user to log in', js: true do
+    user = User.create(:username => 'Bo', :password => 'pw', :password_confirmation => 'pw')
+    visit '/'
+    click_link 'Log In'
+    fill_in 'User name', :with => user.username
+    fill_in 'Password', :with => user.password
+    click_button 'Log In'
+    page.should have_content "Logged in!"
+  end
+
+  it 'throw an error if log in is incorrect', js: true do
+    user = User.create(:username => 'Bo', :password => 'pw', :password_confirmation => 'pw')
+    visit '/'
+    click_link 'Log In'
+    fill_in 'User name', :with => user.username
+    fill_in 'Password', :with => "fgdf"
+    click_button 'Log In'
+    page.should have_content "Username or password is invalid."
+  end
 end
